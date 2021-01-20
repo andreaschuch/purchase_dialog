0cca16d12709548a4a03b0371c6816f0bec66134
Andrea <andrea.schuch@gmail.com>
2021-01-20 16:32:38 -0500

    Implement mulit item purchase using entity groups and custom actions

--- README.md
+++ README.md
@@ -7,11 +7,20 @@ Then start the action server:
 ```shell
 rasa run actions
 ```
-
+or
+```shell script
+python -m rasa_sdk --actions actions
+```
 Run the dialog in test mode or interactive (learning mode):
 ```shell
 rasa shell  --endpoints endpoints.yml
 rasa interactive  --endpoints endpoints.yml
 ```
 
+# Challenges:
+* Slot type list can collect several commodities and several quantities, but it doesn't know which one is which if one is missing
+** https://blog.rasa.com/introducing-entity-roles-and-groups/
+** https://github.com/BeWe11/rasa_composite_entities
+* NLU model for units (e.g. pounds)
+* 
 
--- actions/actions.py
+++ actions/actions.py
@@ -1,50 +1,68 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+# # This files contains your custom actions which can be used to run
+# # custom Python code.
+# #
+# # See this guide on how to implement these action:
+# # https://rasa.com/docs/rasa/custom-actions
 #
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
+#
+# # This is a simple example for a custom action which utters "Hello World!"
+#
+from random import choice
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-from random import choice
+from rasa_sdk.executor import CollectingDispatcher
+
 
 class ActionAnythingElse(Action):
-
     def name(self) -> Text:
         return "action_anything_else"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print(domain)
-
-        commodity = tracker.get_slot('commodity')
 
         price = tracker.get_slot('price')
-        quantity = tracker.get_slot('quantity')
-        new_commodity = {"commodity": commodity, "quantity": quantity, "price": price}
-
         all_commodities = tracker.get_slot('all_commodities')
-        all_commodities.append(new_commodity)
+        commodities_missing_quantities = tracker.get_slot('commodities_missing_quantities')
+        new_commodities = []
+        for group in range(10):
+            try:
+                commodity = next(tracker.get_latest_entity_values(entity_type="commodity", entity_group=str(group)))
+            except StopIteration:
+                commodity = None
+            try:
+                quantity = next(tracker.get_latest_entity_values(entity_type="quantity", entity_group=str(group)))
+            except StopIteration:
+                quantity = None
 
-        dispatcher.utter_message(template = "utter_anything_else")
+            if commodity and quantity:
+                new_commodity = {"commodity": commodity, "quantity": quantity, "price": price}
+                if commodity in commodities_missing_quantities:
+                    commodities_missing_quantities.remove(commodity)
+                new_commodities.append(new_commodity)
+            elif commodity:
+                commodities_missing_quantities.append(commodity)
+            elif quantity:
+                commodity = commodities_missing_quantities.pop(0)
+                new_commodity = {"commodity": commodity, "quantity": quantity, "price": price}
+                new_commodities.append(new_commodity)
 
-        # price_init = domain["slots"]["price"]["initial_value"]
-        commodity_init = domain["slots"]["commodity"]["initial_value"]
-        quantity_init = domain["slots"]["quantity"]["initial_value"]
+        all_commodities.extend(new_commodities)
 
-        # print(price_init)
-        print(commodity_init)
-        print(quantity_init)
+        if commodities_missing_quantities:
+            commodity_missing_quantity = commodities_missing_quantities[0]
+            templates = domain['responses']['utter_ask_quantity']
+            utterance = choice(templates)['text'].format(commodity=commodity_missing_quantity)
+            dispatcher.utter_message(text=utterance)
+        elif new_commodities:
+            dispatcher.utter_message(template="utter_anything_else")
+        else:
+            dispatcher.utter_message(template="utter_ask_commodity")
 
         return [SlotSet("all_commodities", all_commodities),
-                SlotSet('commodity', commodity_init), SlotSet('quantity', quantity_init)] #TODO: SlotSet('price', price_init)
+                SlotSet('commodities_missing_quantities',  commodities_missing_quantities)]
 
 
 class ActionPay(Action):
@@ -54,7 +72,6 @@ class ActionPay(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
-        print(domain)
 
         dispatcher.utter_message(template="utter_thanks")
 
@@ -71,10 +88,11 @@ class ActionPrice(Action):
         print(domain)
 
         all_commodities = tracker.get_slot('all_commodities')
+        print(all_commodities)
         price = 0.0
         for commodity in all_commodities:
             price += float(commodity['price']) * float(commodity['quantity'])
-            print(price)
+            # print(price)
 
         templates = domain['responses']['utter_price']
         utterance = choice(templates)['text'].format(price=price, currency=tracker.get_slot('currency'))
